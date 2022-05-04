eb39efc0ce8aa76259090297d20289d0927d9d5f
Dinh Bao Tran Nguyen <dinhbaotran.nguyen@sjsu.edu>
2022-05-04 00:47:02 -0700

    crud function

--- public/main.js
+++ public/main.js
@@ -1,30 +1,173 @@
-// main.js
-const update = document.querySelector('#update-button');
+// main.js: code for interacting with the client
+const getURL = 'http://localhost:4000/';
+const postURL = 'http://localhost:4000/addAirport';
+const deleteURL = 'http://localhost:4000/deleteAirport/';
 
-update.addEventListener('click', (_) => {
-  fetch('/quotes', {
-    method: 'put',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: 'd',
-      country: 'Canada',
-    }),
+function addData() {
+  const code = document.getElementById('code').value;
+  const name = document.getElementById('name').value;
+  const icao = document.getElementById('icao').value;
+  const type = document.getElementById('type').value;
+  const phone = document.getElementById('phone').value;
+  const email = document.getElementById('email').value;
+  const url = document.getElementById('url').value;
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+  const tz = document.getElementById('tz').value;
+  const country = document.getElementById('country').value;
+  const elev = document.getElementById('elev').value;
+  const lat = document.getElementById('lat').value;
+  const lon = document.getElementById('lon').value;
+  const woeid = document.getElementById('woeid').value;
+  const carriers = document.getElementById('carriers').value;
+  const direct_flights = document.getElementById('direct_flights').value;
+  const runway_length = document.getElementById('runway_length').value;
+
+  if (!code) {
+    alert('Name is required!!');
+    document.getElementById('name').focus();
+    return;
+  }
+  if (!name) {
+    alert('Name is required!!');
+    document.getElementById('name').focus();
+    return;
+  }
+  if (!country) {
+    alert('Country is required!!');
+    document.getElementById('country').focus();
+    return;
+  }
+
+  postToDB({
+    code: code,
+    name: name,
+    icao: icao,
+    phone: phone,
+    email: email,
+    type: type,
+    url: url,
+    city: city,
+    state: state,
+    country: country,
+    elev: elev,
+    tz: tz,
+    lat: lat,
+    lon: lon,
+    woeid: woeid,
+    carriers: carriers,
+    direct_flights: direct_flights,
+    runway_length: runway_length,
   });
-});
+}
 
-const deleteButton = document.querySelector('#delete-button');
-deleteButton.addEventListener('click', (_) => {
-  fetch('/quotes', {
-    method: 'delete',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: 'Anaa Airport',
-    }),
+function postToDB(doc) {
+  fetch(postURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(doc),
   })
     .then((res) => {
-      if (res.ok) return res.json();
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.text().then((text) => {
+          throw new Error(text);
+        });
+      }
     })
-    .then((data) => {
-      window.location.reload();
+    .catch((error) => {
+      console.error('# Error:', error);
+      const msg =
+        'Error: ' +
+        error.message +
+        '. ' +
+        'There was an error posting data to the database. ' +
+        "See browser's console for more details.";
+      document.getElementById('status').innerHTML = msg;
     });
-});
+}
+
+// const update = document.querySelector('#update-button');
+
+// update.addEventListener('click', (_) => {
+//   fetch('/quotes', {
+//     method: 'put',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+//       name: 'd',
+//       country: 'Canada',
+//     }),
+//   });
+// });
+function updateData() {
+  const code = document.getElementById('edit-code').value;
+  const name = document.getElementById('edit-name').value;
+  const icao = document.getElementById('edit-icao').value;
+  const type = document.getElementById('edit-type').value;
+  const phone = document.getElementById('edit-phone').value;
+  const email = document.getElementById('edit-email').value;
+  const url = document.getElementById('edit-url').value;
+  const city = document.getElementById('edit-city').value;
+  const state = document.getElementById('edit-state').value;
+  const tz = document.getElementById('edit-tz').value;
+  const country = document.getElementById('edit-country').value;
+  const elev = document.getElementById('edit-elev').value;
+  const lat = document.getElementById('edit-lat').value;
+  const lon = document.getElementById('edit-lon').value;
+  const woeid = document.getElementById('edit-woeid').value;
+  const carriers = document.getElementById('edit-carriers').value;
+  const direct_flights = document.getElementById('edit-direct_flights').value;
+  const runway_length = document.getElementById('edit-runway_length').value;
+
+  updateToDB({
+    code: code,
+    name: name,
+    icao: icao,
+    phone: phone,
+    email: email,
+    type: type,
+    url: url,
+    city: city,
+    state: state,
+    country: country,
+    elev: elev,
+    tz: tz,
+    lat: lat,
+    lon: lon,
+    woeid: woeid,
+    carriers: carriers,
+    direct_flights: direct_flights,
+    runway_length: runway_length,
+  });
+}
+function updateToDB(doc) {
+  fetch(`/airports/update/${doc._id}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(doc),
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.text().then((text) => {
+          throw new Error(text);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error('# Error:', error);
+      const msg =
+        'Error: ' +
+        error.message +
+        '. ' +
+        'There was an error posting data to the database. ' +
+        "See browser's console for more details.";
+      document.getElementById('status').innerHTML = msg;
+    });
+}
--- server.js
+++ server.js
@@ -1,65 +1,232 @@
 const express = require('express');
+const cors = require('cors');
+const { ObjectId } = require('mongodb');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
-const MongoClient = require('mongodb').MongoClient;
 app.use('/public', express.static('./public/'));
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 const connectionString =
-  'mongodb+srv://root:root@cluster0.6qegf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+  'mongodb+srv://root:root@cluster0.6qegf.mongodb.net/test?retryWrites=true&w=majority';
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then((client) => {
-    console.log('Connected to Database');
-    app.listen(3000);
-    const db = client.db('test');
-    const airportsCollection = db.collection('airports');
+// MongoClient.connect(connectionString, { useUnifiedTopology: true })
+//   .then((client) => {
+//     console.log('Connected to Database');
+//     app.listen(3000);
+//     const db = client.db('test');
+//     const airportsCollection = db.collection('airports');
 
-    app.get('/', (req, res) => {
-      airportsCollection
-        .find()
-        .toArray()
-        .then((results) => {
-          res.render('index.ejs', { airportsArray: results });
-        })
-        .catch(/* ... */);
-    });
+//     app.get('/', (req, res) => {
+//       airportsCollection
+//         .find()
+//         .toArray()
+//         .then((results) => {
+//           res.render('index.ejs', { airportsArray: results });
+//         })
+//         .catch(/* ... */);
+//     });
 
-    app.post('/quotes', (req, res) => {
-      airportsCollection
-        .insertOne(req.body)
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((error) => console.error(error));
-    });
-    app.put('/quotes', (req, res) => {
-      airportsCollection
-        .findOneAndUpdate(
-          { name: 'd' },
-          {
-            $set: {
-              name: req.body.name,
-              country: req.body.country,
-            },
-          },
-          {
-            upsert: true,
-          }
-        )
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((error) => console.error(error));
-    });
+//     app.post('/quotes', (req, res) => {
+//       airportsCollection
+//         .insertOne(req.body)
+//         .then((result) => {
+//           console.log(result);
+//         })
+//         .catch((error) => console.error(error));
+//     });
+//     app.put('/quotes', (req, res) => {
+//       airportsCollection
+//         .findOneAndUpdate(
+//           { name: 'd' },
+//           {
+//             $set: {
+//               name: req.body.name,
+//               country: req.body.country,
+//             },
+//           },
+//           {
+//             upsert: true,
+//           }
+//         )
+//         .then((result) => {
+//           console.log(result);
+//         })
+//         .catch((error) => console.error(error));
+//     });
 
-    app.delete('/quotes', (req, res) => {
-      airportsCollection
-        .deleteOne({ name: req.body.name })
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((error) => console.error(error));
+//     app.delete('/quotes', (req, res) => {
+//       airportsCollection
+//         .deleteOne({ name: req.body.name })
+//         .then((result) => {
+//           console.log(result);
+//         })
+//         .catch((error) => console.error(error));
+//     });
+//   })
+//   .catch((error) => console.error(error));
+
+mongoose.connect(connectionString);
+
+const airportSchema = {
+  code: String,
+  name: String,
+  icao: String,
+  type: String,
+  phone: String,
+  email: String,
+  url: String,
+  city: String,
+  state: String,
+  country: String,
+  elev: String,
+  lat: String,
+  lon: String,
+  tz: String,
+  woeid: String,
+  carriers: String,
+  direct_flights: String,
+  runway_length: String,
+};
+
+const Airport = mongoose.model('Airport', airportSchema);
+
+// get all airports
+app.get('/airports', (req, res) => {
+  Airport.find({}, function (err, airports) {
+    res.render('index.ejs', {
+      airportsArray: airports,
     });
-  })
-  .catch((error) => console.error(error));
+  });
+});
+
+// Get single airport by id
+app.get('/airports/show/:id', (req, res) => {
+  Airport.findOne({ _id: req.params.id }, function (err, airport) {
+    if (err) {
+      console.log('Error: ', err);
+    } else {
+      res.render('show.ejs', {
+        airport: airport,
+      });
+    }
+  });
+});
+
+// add new airport
+app.post('/addAirport', async (req, res) => {
+  let data = req.body;
+  try {
+    data = await Airport.create(data);
+    res.json(data);
+  } catch (err) {
+    console.error('# Post Error', err);
+    res.status(500).send({ error: err.name + ', ' + err.message });
+  }
+});
+
+app.get('/airports/:id/edit', (req, res) => {
+  Airport.findOne({ _id: req.params.id }, (error, airport) => {
+    if (error) {
+      res.redirect('/airports');
+    } else {
+      res.render('edit.ejs', { airport: airport });
+    }
+  });
+});
+
+// update new airport
+// app.post('/airports/update/:id', async (req, res) => {
+//   let data = req.body;
+//   console.log(data);
+//   try {
+//     data = await Airport.findByIdAndUpdate(req.params.id, data);
+
+//     res.redirect('/airports/show/' + req.params.id);
+//   } catch (err) {
+//     console.error('# Post Error', err);
+//     res.status(500).send({ error: err.name + ', ' + err.message });
+//   }
+// });
+app.post('/airports/update/:id', (req, res) => {
+  Airport.findOneAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        code: req.body.code,
+        name: req.body.name,
+        icao: req.body.icao,
+        type: req.body.type,
+        phone: req.body.phone,
+        email: req.body.email,
+        url: req.body.url,
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.country,
+        elev: req.body.elev,
+        tz: req.body.tz,
+        lat: req.body.lat,
+        lon: req.body.lon,
+        woeid: req.body.woeid,
+        carriers: req.body.carriers,
+        direct_flights: req.body.direct_flights,
+        runway_length: req.body.runway_length,
+      },
+    },
+    { new: true },
+    function (err, airport) {
+      if (err) {
+        console.log(err);
+        res.render('edit.ejs', { airport: req.body });
+      } else {
+        res.redirect('/airports/show/' + airport._id);
+      }
+    }
+  );
+});
+
+//UPDATE ROUT
+// app.get('/airports/:id', (req, res) => {
+//   Airport.findByIdAndUpdate(req.params.id, req.body, (err, airport) => {
+//     if (err) {
+//       console.log(err);
+//       res.render('edit.ejs', { airport: req.body });
+//     } else {
+//       res.redirect('/airports/show/' + airport._id);
+//     }
+//   });
+// });
+
+// delete - method 1
+app.get('/airports/:id/delete', (req, res) => {
+  console.log('# Delete Airport', req.params.id);
+  Airport.findByIdAndRemove(req.params.id, (error) => {
+    if (error) {
+      res.redirect('/airports');
+    } else {
+      res.redirect('/airports');
+    }
+  });
+});
+
+// delete inside edit.ejs - method 2
+app.delete('/airports/:id/delete', function (req, res) {
+  Airport.remove({ _id: req.params.id }, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Airport deleted!');
+      res.redirect('/airports');
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  res.redirect('/airports');
+});
+
+app.listen(4000, function () {
+  console.log('server is running');
+});
